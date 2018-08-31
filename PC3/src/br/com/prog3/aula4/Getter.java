c00a434f819456d5f82b434b5155d02f1b2e0d74
Virgilio <virgilios.cc@gmail.com>
2018-08-30 21:09:00 -0300

    Arrumando a questões da aula 4, adicionando as annotations

---
+++ PC3/src/br/com/prog3/aula4/Getter.java
@@ -0,0 +1,5 @@
+package br.com.prog3.aula4;
+
+public @interface Getter {
+
+}
--- PC3/src/br/com/prog3/aula4/Veiculo.java
+++ PC3/src/br/com/prog3/aula4/Veiculo.java
@@ -3,13 +3,15 @@ package br.com.prog3.aula4;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity 
 public class Veiculo {
+	@Persistent
 	protected String descricaoVeiculo = null;
-
+	@Getter
 	public String getDescricaoVeiculo() {
 		return descricaoVeiculo;
 	}
-
+	
 	public void setDescricaoVeiculo(String descricaoVeiculo) {
 		this.descricaoVeiculo = descricaoVeiculo;
 	}
